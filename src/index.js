f43633089d9ff3dafb5b96a508790f6926da383c
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-18 15:21:50 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import Button from '~/components/Button';
+// import { Container } from './styles';
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Button />
+    </SafeAreaView>
+  );
+}
