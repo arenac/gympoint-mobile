6eadf740496338a8f6a1e491a907342be2247ec3
Nilo Neregato <niloneregato@hotmail.com>
2019-12-22 19:00:06 +0100

    remove unused component

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Image } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
