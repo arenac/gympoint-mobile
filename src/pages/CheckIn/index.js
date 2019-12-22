0dcf88be9162b06e087278cfabae069c3af09213
Nilo Neregato <niloneregato@hotmail.com>
2019-12-22 16:03:28 +0100

    CheckIns tab icon

--- src/pages/CheckIn/index.js
+++ src/pages/CheckIn/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 // import { Container } from './styles';
 
 export default function CheckIn({ navigation }) {
@@ -10,3 +12,11 @@ export default function CheckIn({ navigation }) {
 
   return <View />;
 }
+
+
+CheckIn.navigationOptions = {
+  tabBarLabel: 'Dashboard',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="check-circle" size={20} color={tintColor} />
+  ),
+};
